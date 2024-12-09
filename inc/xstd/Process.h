d2f59ef491314144dc9465480b823e09c2446f16
Deviatt <borodinm744@gmail.com>
2024-12-10 01:53:55 +0300

    [✨] Added XSleep

--- inc/xstd/Process.h
+++ inc/xstd/Process.h
@@ -4,10 +4,14 @@
 
 #ifdef _WIN32
 	void (ExitProcess) 	(uint32_t);
+	void (Sleep)		(uint32_t);
+
 	#define XExit(code) do { \
 			ExitProcess((code)); \
 			XUNREACH(); \
 		} while (0);
+
+	#define XSleep(ms) (Sleep((ms)))
 #else
 	#include <xstd/System/SystemCall.h>
 
@@ -16,5 +20,15 @@
 			(void)syscall1(SYS_EXIT_GROUP, (code)); \
 			XUNREACH(); \
 		} while (0);
+
+		struct __timespec {
+			time_t 		sec;
+			long_t 		nsec;
+		};
+
+		static XFINL void XSleep(uint32_t ms) {
+			struct __timespec ts = {(time32_t)(ms / 1000), (time32_t)(ms % 1000) * 1000UL};
+			syscall2(SYS_NANOSLEEP, &ts, NULL);
+		}
 	#endif
 #endif
